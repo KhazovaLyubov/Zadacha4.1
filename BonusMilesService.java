2fd56c6e4398bb4483fedd4be89bc47119ac6544
Khazova Lyubov <lyubashilina@rambler.ru>
2020-09-23 16:49:28 +0400

    Added BonusMilesService.java

---
+++ BonusMilesService.java
@@ -0,0 +1,10 @@
+public class BonusMilesService {
+    public int calculate(int cost, int miles) {
+        int amount = 20;
+        int sum= 10_000;
+        int mile = sum / amount;
+         return mile;
+    }
+    }
+
+        
